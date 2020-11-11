aeb8d83e75155bf9e10c04234b4b80f17589215c
Andrew Eames <ajerxs@gmail.com>
2020-11-11 15:41:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,10 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  calls_long.any? do |call|
+    return true if call.size > 4
+  end
 end
 
 def find_the_cheese# code an argument here
