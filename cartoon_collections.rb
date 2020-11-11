c3e9e6e46ba84f4335e28219b4f1044f8dfabeb6
Andrew Eames <ajerxs@gmail.com>
2020-11-11 15:09:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |dwarf|
+  dwarves.collect do |dwarf|
+    dwarf.each_with_index
 end
 
 def summon_captain_planet# code an argument here
